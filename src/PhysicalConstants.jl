b2c3f9c776349c86583aa393f26cc69b895347f1
schrist <SimonChrist@gmx.de>
2018-03-20 15:40:53 +0100

    setup types for dispatch

--- src/PhysicalConstants.jl
+++ src/PhysicalConstants.jl
@@ -1,4 +1,10 @@
 module PhysicalConstants
+    using Unitful
+
+    abstract type PhysicalConstant <: Number end
+    struct MKSConstant <: PhysicalConstant end
+    struct CGSConstant <: PhysicalConstant end
+    
     include("CGS.jl")
     include("Imperial.jl")
     include("MKS.jl")
