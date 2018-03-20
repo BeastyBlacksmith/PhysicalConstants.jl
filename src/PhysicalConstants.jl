de5a7802d3d2fb7b99f2d109dbf40edf3ba616ad
schrist <SimonChrist@gmx.de>
2018-03-20 23:10:12 +0100

    MKS unitized.

--- src/PhysicalConstants.jl
+++ src/PhysicalConstants.jl
@@ -1,11 +1,31 @@
 module PhysicalConstants
-    using Unitful
+using Unitful
 
-    abstract type PhysicalConstant <: Number end
-    struct MKSConstant <: PhysicalConstant end
-    struct CGSConstant <: PhysicalConstant end
-    
-    include("CGS.jl")
-    include("Imperial.jl")
-    include("MKS.jl")
+export switch_units, MKSConstant
+const HAS_UNITS = Ref(false)
+
+switch_units() = (global HAS_UNITS; HAS_UNITS[] = !HAS_UNITS[]; return nothing)
+
+abstract type PhysicalConstant <: Number end
+struct MKSConstant{ V <: Unitful.Quantity{ <: Real } } <: PhysicalConstant
+    value::V
+end
+
+function (c::MKSConstant)(;units=HAS_UNITS[])
+    units || return ustrip( c.value )
+    return c.value
+end
+
+struct CGSConstant{ V <: Unitful.Quantity{ <: Real } } <: PhysicalConstant
+    value::V
+end
+
+function (c::CGSConstant)(;units=HAS_UNITS[])
+    units || return ustrip( c.value )
+    return c.value
+end
+
+include("CGS.jl")
+include("Imperial.jl")
+include("MKS.jl")
 end
